c50effc462872f15769a86d04fbaf2943f48f28f
Brian Michel <brian.michel@gmail.com>
2012-07-01 15:28:52 -0400

    Initial Commit

---
+++ Tumblr/Tumblr.h
@@ -0,0 +1,13 @@
+//
+//  Tumblr.h
+//  Tumblr
+//
+//  Created by Brian Michel on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tumblr : NSObject
+
+@end
---
+++ Tumblr/Tumblr.m
@@ -0,0 +1,13 @@
+//
+//  Tumblr.m
+//  Tumblr
+//
+//  Created by Brian Michel on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tumblr.h"
+
+@implementation Tumblr
+
+@end
